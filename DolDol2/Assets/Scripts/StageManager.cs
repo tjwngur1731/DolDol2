baedf8f37aef8048beea595817492323f4b5519e
Yunju J <68594937+18yjang@users.noreply.github.com>
2020-10-24 01:07:07 +0900

    Scene이동 완성
    Scene 분할 및 이동 가능,
    스테이지 선택버튼 임의로 구현(이동),
    일시정지기능(ESC버튼), 메인화면 게임종료버튼 구현

---
+++ DolDol2/Assets/Scripts/StageManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// 모든 스테이지에 공통으로 쓰일 기능들 여기 넣어주세요!
+
+public class StageManager : MonoBehaviour
+{
+    public bool paused = false;     //일시정지
+
+    public GameObject UIOption;     //일시정지창
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // 일시정지 구현
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                paused = !paused;
+            }
+            if (paused)
+            {
+                UIOption.SetActive(true);
+                Time.timeScale = 0;
+                Time.fixedDeltaTime = 0.02f * Time.timeScale;
+            }
+            else
+            {
+                UIOption.SetActive(false);
+                Time.timeScale = 1;
+            }
+
+        // 
+
+
+
+
+
+        // if - clear - switch stage(1,2,3) - SceneManager.LoadScene("Stage2 / 3 /4 ---");               // finish 닿으면 바로 다음 스테이지로 이동
+    }
+
+
+    public void OnClickButton() //버튼 클릭하면 이동
+    {
+        SceneManager.LoadScene(gameObject.name);
+    }
+
+    public void OnClickContinue()
+    {
+        paused = !paused;
+    }
+}
---
+++ DolDol2/Assets/Scripts/StageSelect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageSelect : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+
+    public void OnClickButton() //버튼 클릭하면 이동
+    {
+            SceneManager.LoadScene(gameObject.name);
+    }
+
+}
