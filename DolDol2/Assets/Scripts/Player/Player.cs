5bc0677b9b4c96de170103179c0b5333f8abdae0
sojunyoup <thwnsuq46@naver.com>
2020-10-24 16:45:24 +0900

    캐릭터발판

---
+++ DolDol2/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform spawnPos;
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            gameObject.transform.position = spawnPos.position;
+            Debug.Log("asf");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DolDol2/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    [SerializeField]
+    private float speed = 1.0f;
+    public float jumpPower = 1.0f;
+    [SerializeField]
+    private bool isGround = false;
+    [SerializeField]
+    Transform pos;
+    [SerializeField]
+    float checkRadius;
+    [SerializeField]
+    LayerMask islayer;
+
+    SpriteRenderer renderer;
+
+    public float runDelay;
+    float curDelay;
+
+    bool runCount;
+    public Sprite[] runSprite;
+
+    public Sprite jumpSprite;
+    public Sprite staySprite;
+
+    //움직이는 중인가.
+    bool isrunnig = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+
+        renderer = GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        if (isrunnig == true)
+        {
+            curDelay += Time.deltaTime;
+            if (curDelay >= runDelay)
+            {
+                if (runCount)
+                {
+                    runCount = !runCount;
+                    renderer.sprite = runSprite[0];
+                }
+                else if (!runCount)
+                {
+                    runCount = !runCount;
+                    renderer.sprite = runSprite[1];
+                }
+                curDelay = 0;
+            }
+        }
+        //바닥체크 점프
+        isGround = Physics2D.OverlapCircle(pos.position, checkRadius, islayer);
+        if (Input.GetKey(KeyCode.W) && isGround == true)
+        {
+            rigid.velocity = Vector2.up * jumpPower;
+        }
+
+        if (!isGround)
+        {
+            renderer.sprite = jumpSprite;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            isrunnig = true;
+            renderer.flipX = false;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            isrunnig = true;
+            renderer.flipX = true;
+        }
+        else if(isGround)
+        {
+            renderer.sprite = staySprite;
+        }
+        else
+        {
+            isrunnig = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = transform.position;
+        //좌우 이동
+        float hor = Input.GetAxis("Horizontal");
+
+        rigid.velocity = new Vector2(hor * speed, rigid.velocity.y);
+
+        transform.position = position;
+    }
+}
