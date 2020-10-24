3f98635e357b1d36239ec46156c23781c06d8f31
tjwngur1731 <1sjhjs1@gmail.com>
2020-10-25 01:26:38 +0900

    star UI

--- DolDol2/Assets/Scripts/Star/Star.cs
+++ DolDol2/Assets/Scripts/Star/Star.cs
@@ -1,14 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Star : MonoBehaviour
 {
+
+    public Image[] starUI;
+
+    private void Start()
+    {
+        GameManager.Instance.starCount = 0;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             Destroy(gameObject);
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump")) GameManager.Instance.starCount++;
+        StarUp(GameManager.Instance.starCount);
+    }
+
+    void StarUp(int cnt)
+    {
+        for (int i = 0; i < cnt; i++)
+        {
+            starUI[i].gameObject.SetActive(true);
         }
     }
 }
