5d275383f373d86ad168cc7d0681b6ed233246fc
서문 규 <illsi@naver.com>
2020-11-08 17:55:41 +0900

    작성중

--- DolDol2/Assets/Scripts/CustomEditorTest.cs
+++ DolDol2/Assets/Scripts/CustomEditorTest.cs
@@ -5,11 +5,13 @@ using System.Collections;
 public class CustomEditorTest : EditorWindow {
 
     public Rect windowRect = new Rect(100, 100, 200, 200);
-    public Texture texture; //Image_Ground 1_0
+    private Texture texture; //Image_Ground 1_0
     void OnGUI() {
         BeginWindows();
-        texture = (Texture)Resources.Load("Assets/Images/Floor/Image_Ground 1_0.asset");
-        GUILayout.Button(texture);
+        texture = (Texture)Resources.Load("Assets/Images/Button_Resume.png");
+        GUILayout.Button(texture, GUILayout.Width(70), GUILayout.Height(70));
+        GUILayout.Label("Test");
+        // GUILayout.Button("texture", GUILayout.Width(70), GUILayout.Height(70));
         EndWindows();
     }
 
