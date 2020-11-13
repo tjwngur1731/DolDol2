fafa261a8f06f9e9f2f39fa8744047567a456517
misterq <illsi@naver.com>
2020-11-13 23:55:17 +0900

    맵 에디터 마무리 수정 작업중

--- DolDol2/Assets/Scripts/CustomEditorTest.cs
+++ DolDol2/Assets/Scripts/CustomEditorTest.cs
@@ -4,207 +4,370 @@ using System.Collections;
 using System.IO;
 using System;
 
-public class CustomEditorTest : EditorWindow {
-    public Rect windowRect = new Rect(100, 100, 200, 200);
-    private Vector3 mousepos;
-    private int[,] fieldMap = null;
-    private bool isActive = false;
-    private float TileInterval = 1.8f;
-    private float minifildInterval = 9.0f;
-    private GameObject root = null;
-    private GameObject[,] minifield = null;
-    private int type = -1;
-    void OnGUI() {
+public class CustomEditorTest : EditorWindow
+{
+  public Rect windowRect = new Rect(100, 100, 200, 200);
+  private Vector3 mousepos;
+  private int[,] fieldMap = null;
+  private BaseObject[,] baseObjectFieldMap = null;
+  private bool isActive = false;
+  private float TileInterval = 1.8f;
+  private float minifildInterval = 9.0f;
+  private GameObject root = null;
+  private GameObject[,] minifield = null;
+  private int type = -1;
+  void OnGUI()
+  {
 
-        BeginWindows();
-        
-        if (GUILayout.Button("", GUILayout.Width(50), GUILayout.Height(40)))
-        {
-            type = 2;
-        }
-        GUILayout.Label("Wall");
+    BeginWindows();
 
-        if (GUILayout.Button("", GUILayout.Width(50), GUILayout.Height(40)))
-        {
-            type = 0;
-        }
-        GUILayout.Label("Player1");
-        
-        if (GUILayout.Button("", GUILayout.Width(50), GUILayout.Height(40)))
-        {
-            type = 1;
-        }
-        GUILayout.Label("Player2");
-
-        if (GUILayout.Button("Save", GUILayout.Width(50), GUILayout.Height(40)))
-        {
-            SaveToFile("SaveTest");
-        }
-
-        GUILayout.Label("Wall");
-
-        EndWindows();
-    }
-
-    void OnEnable() 
+    var wallTex = ((Resources.Load("Prefab/Wall") as GameObject).GetComponent<SpriteRenderer>()).sprite.texture;
+    if (GUILayout.Button(wallTex, GUILayout.Width(50), GUILayout.Height(40)))
     {
-        SceneView.duringSceneGui += OnSceneGUI;
+      type = 2;
     }
+    GUILayout.Label("Wall");
 
-    void OnDisable() 
+    var player1Tex = ((Resources.Load("Prefab/Player 1") as GameObject).GetComponent<SpriteRenderer>()).sprite.texture;
+    if (GUILayout.Button(player1Tex, GUILayout.Width(50), GUILayout.Height(40)))
     {
-        SceneView.duringSceneGui -= OnSceneGUI;
+      type = 0;
     }
+    GUILayout.Label("Player1");
 
-    void OnDestory()
+    var player2Tex = ((Resources.Load("Prefab/Player 2") as GameObject).GetComponent<SpriteRenderer>()).sprite.texture;
+    if (GUILayout.Button(player2Tex, GUILayout.Width(50), GUILayout.Height(40)))
     {
-        Destroy(root);
+      type = 1;
     }
+    GUILayout.Label("Player2");
 
-    void OnFocus()
+    if (GUILayout.Button("Save", GUILayout.Width(50), GUILayout.Height(40)))
     {
-        isActive = true;
+      SaveToFile("SaveTest");
     }
 
-    void OnLostFocus()
+    GUILayout.Label("Wall");
+
+    EndWindows();
+  }
+
+  void OnEnable()
+  {
+    SceneView.duringSceneGui += OnSceneGUI;
+  }
+
+  void OnDisable()
+  {
+    SceneView.duringSceneGui -= OnSceneGUI;
+  }
+
+  void OnDestory()
+  {
+    Destroy(root);
+  }
+
+  void OnFocus()
+  {
+    isActive = true;
+  }
+
+  void OnLostFocus()
+  {
+    isActive = false;
+  }
+
+  void SaveToFile(string stage)
+  {
+    string filename = "CellMap/" + stage + ".cellmap";
+
+    using (StreamWriter outputFile = new StreamWriter(@filename))
     {
-        isActive = false;
-    }
+      for (int i = 25; i >= 1; i--)
+      {
+        string line = "";
+        for (int j = 1; j <= 25; j++)
+        {
+          // Debug.Log(fieldMap[i + 1,j + 1]);
+          int ch = fieldMap[i, j];
+          if (ch == ' ')
+          {
+            line += (char)ch;
+          }
+          else
+          {
+            line += ch;
+          }
+        }
 
-    void SaveToFile(string stage)
+        outputFile.WriteLine(line);
+      }
+    }
+  }
+
+  void OnSceneGUI(SceneView sceneView)
+  {
+    // if (isActive != true)
+    // {
+    //     return;
+    // }
+
+    Event current = Event.current;
+
+    if (root == null)
     {
-        string filename = "CellMap/" + stage + ".cellmap";
-
-        using (StreamWriter outputFile = new StreamWriter(@filename))
-        {
-            for (int i = 25; i >= 1; i--)
-            {
-                string line = "";
-                for (int j = 1; j <= 25; j++)
-                {
-                    // Debug.Log(fieldMap[i + 1,j + 1]);
-                    int ch = fieldMap[i,j];
-                    if (ch == ' ')
-                    {
-                        line += (char)ch;
-                    }
-                    else 
-                    {
-                        line += ch;
-                    }
-                }
-
-                outputFile.WriteLine(line);
-            }
-        }
+      root = new GameObject();
+      root.name = "MapEditorRoot";
     }
 
-    void OnSceneGUI(SceneView sceneView) {
-        // if (isActive != true)
-        // {
-        //     return;
-        // }
-
-        Event current = Event.current;
-
-        if (root == null)
-        {
-            root = new GameObject();
-            root.name = "MapEditorRoot";
-        }
-
-        if (minifield == null)
-        {
-            minifield = new GameObject[5, 5];
-
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    minifield[i,j] = new GameObject();
-                    minifield[i,j].name = "minifield_" + i + "_" + j;
-                    minifield[i,j].transform.position = new Vector2(j * 5 * TileInterval, i * 5 * TileInterval);
-                    minifield[i,j].transform.SetParent(root.transform);
-                }   
-            }
-        }
-
-        if (fieldMap == null)
-        {
-            fieldMap = new int[27,27];
-
-            for (int i = 0; i < 27; i++)
-            {
-                for (int j = 0; j < 27; j++)
-                {
-                    fieldMap[i,j] = ' ';
-                }   
-            }
-        }
-
-        if (current.type == EventType.MouseDown && current.button == 0) {
-            Camera cam = SceneView.lastActiveSceneView.camera;
- 
-            mousepos = current.mousePosition;
-            mousepos.y = Screen.height - mousepos.y - 36.0f;
-            mousepos = cam.ScreenToWorldPoint (mousepos);
-            mousepos.z = 0;
-
-            AddObject(mousepos);
-        }
-    }
-
-    void AddObject(Vector3 pos)
+    if (minifield == null)
     {
-        int indexJ = (int)(Math.Round(pos.x) / minifildInterval);
-        int indexI = (int)(Math.Round(pos.y) / minifildInterval);
+      minifield = new GameObject[5, 5];
 
-        if (!((indexJ >= 0 && indexJ <= 5) && (indexI >= 0 && indexI <= 5)))
+      for (int i = 0; i < 5; i++)
+      {
+        for (int j = 0; j < 5; j++)
         {
-            return;
+          minifield[i, j] = new GameObject();
+          minifield[i, j].name = "minifield_" + i + "_" + j;
+          minifield[i, j].transform.position = new Vector2(j * 5 * TileInterval, i * 5 * TileInterval);
+          minifield[i, j].transform.SetParent(root.transform);
         }
-
-        GameObject obj = null;
-
-        switch(type)
-        {
-            case 0:
-                obj = Instantiate(Resources.Load("Prefab/Player 1")) as GameObject;
-            break;
-
-            case 1:
-                obj = Instantiate(Resources.Load("Prefab/Player 2")) as GameObject;
-            break;
-
-            case 2:
-                obj = Instantiate(Resources.Load("Prefab/Wall")) as GameObject;
-            break;
-        }
-
-        if (obj == null)
-        {
-            return;
-        }
-
-        pos.x = (int)((pos.x + TileInterval / 2) / TileInterval) * TileInterval;
-        pos.y = (int)((pos.y + TileInterval / 2) / TileInterval) * TileInterval;
-        
-        // if (fieldMap[fieldIndexI, fieldIndexJ] != ' ')
-        // {
-        //     fieldMap[fieldIndexI, fieldIndexJ] = ' ';
-        // }
-
-        int fieldIndexI = ((int)Math.Round(pos.y / TileInterval) + 1);
-        int fieldIndexJ = ((int)Math.Round(pos.x / TileInterval) + 1);
-
-        fieldMap[fieldIndexI, fieldIndexJ] = type;
-        
-        obj.transform.position = pos;
-        obj.transform.SetParent(minifield[indexI, indexJ].transform);
+      }
     }
 
-    [MenuItem("MapEditor/CustomEditorTest")]
-    static void Init() {
-        EditorWindow.GetWindow(typeof(CustomEditorTest));
+    if (fieldMap == null)
+    {
+      fieldMap = new int[27, 27];
+
+      for (int i = 0; i < 27; i++)
+      {
+        for (int j = 0; j < 27; j++)
+        {
+          fieldMap[i, j] = ' ';
+        }
+      }
     }
+
+    if (baseObjectFieldMap == null)
+    {
+      baseObjectFieldMap = new BaseObject[27, 27];
+
+      for (int i = 0; i < 27; i++)
+      {
+        for (int j = 0; j < 27; j++)
+        {
+          baseObjectFieldMap[i, j] = null;
+        }
+      }
+    }
+
+    Camera cam = SceneView.lastActiveSceneView.camera;
+
+    mousepos = current.mousePosition;
+    mousepos.y = Screen.height - mousepos.y - 36.0f;
+    mousepos = cam.ScreenToWorldPoint(mousepos);
+    mousepos.z = 0;
+
+    if (current.type == EventType.MouseDown && current.button == 0)
+    {
+    
+      AddObject(mousepos);
+    }
+  }
+
+  void AddObject(Vector3 pos)
+  {
+    int indexJ = (int)(Math.Round(pos.x) / minifildInterval);
+    int indexI = (int)(Math.Round(pos.y) / minifildInterval);
+
+    if (!((indexJ >= 0 && indexJ <= 5) && (indexI >= 0 && indexI <= 5)))
+    {
+      return;
+    }
+
+    pos.x = (int)((pos.x + TileInterval / 2) / TileInterval) * TileInterval;
+    pos.y = (int)((pos.y + TileInterval / 2) / TileInterval) * TileInterval;
+
+    int fieldIndexI = ((int)Math.Round(pos.y / TileInterval) + 1);
+    int fieldIndexJ = ((int)Math.Round(pos.x / TileInterval) + 1);
+
+    if (fieldMap[fieldIndexI, fieldIndexJ] != ' ')
+    {
+      fieldMap[fieldIndexI, fieldIndexJ] = ' ';
+      baseObjectFieldMap[fieldIndexI, fieldIndexJ] = null;
+    }
+
+    GameObject obj = null;
+
+    switch (type)
+    {
+      case 0:
+        obj = Instantiate(Resources.Load("Prefab/Player 1")) as GameObject;
+        break;
+
+      case 1:
+        obj = Instantiate(Resources.Load("Prefab/Player 2")) as GameObject;
+        break;
+
+      case 2:
+        obj = Instantiate(Resources.Load("Prefab/Wall")) as GameObject;
+        RemapWallTexture(obj.GetComponent<BaseObject>(), fieldIndexI, fieldIndexJ);
+
+        break;
+    }
+
+    if (obj == null)
+    {
+      return;
+    }
+
+    fieldMap[fieldIndexI, fieldIndexJ] = type;
+    baseObjectFieldMap[fieldIndexI, fieldIndexJ] = obj.GetComponent<BaseObject>();
+
+    if (type == 2)
+    {
+        RemapWallTexture(baseObjectFieldMap[fieldIndexI - 1, fieldIndexJ], fieldIndexI - 1, fieldIndexJ);
+        RemapWallTexture(baseObjectFieldMap[fieldIndexI + 1, fieldIndexJ], fieldIndexI + 1, fieldIndexJ);
+        RemapWallTexture(baseObjectFieldMap[fieldIndexI, fieldIndexJ - 1], fieldIndexI, fieldIndexJ - 1);
+        RemapWallTexture(baseObjectFieldMap[fieldIndexI, fieldIndexJ + 1], fieldIndexI, fieldIndexJ + 1);
+    }
+
+    obj.transform.position = pos;
+    obj.transform.SetParent(minifield[indexI, indexJ].transform);
+  }
+
+  [MenuItem("MapEditor/CustomEditorTest")]
+  static void Init()
+  {
+    EditorWindow.GetWindow(typeof(CustomEditorTest));
+  }
+
+  void RemapWallTexture(BaseObject baseObj, int i, int j)
+  {
+    if (baseObj == null)
+    {
+        return;
+    }
+
+    Wall wall = (baseObj as Wall);
+
+    int down = fieldMap[i - 1, j];
+    int up = fieldMap[i + 1, j];
+    int left = fieldMap[i, j - 1];
+    int right = fieldMap[i, j + 1];
+
+    // 0칸
+    if ((up == 2 && down == 2) &&
+        (left == 2 && right == 2))
+    {
+      wall.SetWallType(10);
+    }
+
+    // 1칸
+    if ((up != 2 && down == 2) &&
+        (left == 2 && right == 2))
+    {
+      wall.SetWallType(0);
+    }
+
+    if ((up == 2 && down != 2) &&
+        (left == 2 && right == 2))
+    {
+      wall.SetWallType(0);
+      baseObj.transform.eulerAngles = new Vector3(baseObj.transform.eulerAngles.x, baseObj.transform.eulerAngles.y, baseObj.transform.eulerAngles.z + 180.0f);
+    }
+
+    if ((up == 2 && down == 2) &&
+        (left != 2 && right == 2))
+    {
+      wall.SetWallType(0);
+      baseObj.transform.eulerAngles = new Vector3(baseObj.transform.eulerAngles.x, baseObj.transform.eulerAngles.y, baseObj.transform.eulerAngles.z + 90.0f);
+    }
+
+    if ((up == 2 && down == 2) &&
+        (left == 2 && right != 2))
+    {
+      wall.SetWallType(0);
+      baseObj.transform.eulerAngles = new Vector3(baseObj.transform.eulerAngles.x, baseObj.transform.eulerAngles.y, baseObj.transform.eulerAngles.z - 90.0f);
+    }
+
+    // 2칸
+    if ((up == 2 && down == 2) &&
+        (left != 2 && right != 2))
+    {
+      wall.SetWallType(9);
+      baseObj.transform.eulerAngles = new Vector3(baseObj.transform.eulerAngles.x, baseObj.transform.eulerAngles.y, 90.0f);
+    }
+
+    if ((up != 2 && down != 2) &&
+        (left == 2 && right == 2))
+    {
+      wall.SetWallType(9);
+    }
+
+    if ((up == 2 && down != 2) &&
+        (left == 2 && right != 2))
+    {
+      wall.SetWallType(3);
+      baseObj.transform.eulerAngles = new Vector3(baseObj.transform.eulerAngles.x, baseObj.transform.eulerAngles.y, 180.0f);
+    }
+
+    if ((up == 2 && down != 2) &&
+        (left != 2 && right == 2))
+    {
+      wall.SetWallType(3);
+      baseObj.transform.eulerAngles = new Vector3(baseObj.transform.eulerAngles.x, baseObj.transform.eulerAngles.y, 90.0f);
+    }
+
+    if ((up != 2 && down == 2) &&
+        (left == 2 && right != 2))
+    {
+      wall.SetWallType(3);
+      baseObj.transform.eulerAngles = new Vector3(baseObj.transform.eulerAngles.x, baseObj.transform.eulerAngles.y, 90.0f);
+    }
+
+    if ((up != 2 && down == 2) &&
+        (left != 2 && right == 2))
+    {
+      wall.SetWallType(3);
+      baseObj.transform.eulerAngles = new Vector3(baseObj.transform.eulerAngles.x, baseObj.transform.eulerAngles.y, 180.0f);
+    }
+
+    // 3개
+    if ((up != 2 && down != 2) &&
+        (left != 2 && right == 2))
+    {
+      wall.SetWallType(4);
+      baseObj.transform.eulerAngles = new Vector3(baseObj.transform.eulerAngles.x, baseObj.transform.eulerAngles.y, 0);
+    }
+
+    if ((up != 2 && down != 2) &&
+        (left == 2 && right != 2))
+    {
+      wall.SetWallType(4);
+      baseObj.transform.eulerAngles = new Vector3(baseObj.transform.eulerAngles.x, baseObj.transform.eulerAngles.y, 180);
+    }
+
+    if ((up != 2 && down == 2) &&
+        (left != 2 && right != 2))
+    {
+      wall.SetWallType(4);
+      baseObj.transform.eulerAngles = new Vector3(baseObj.transform.eulerAngles.x, baseObj.transform.eulerAngles.y, -90.0f);
+    }
+
+    if ((up == 2 && down != 2) &&
+        (left != 2 && right != 2))
+    {
+      wall.SetWallType(4);
+      baseObj.transform.eulerAngles = new Vector3(baseObj.transform.eulerAngles.x, baseObj.transform.eulerAngles.y, 90.0f);
+    }
+
+    // 4개
+    if ((up != 2 && down != 2) &&
+        (left != 2 && right != 2))
+    {
+      wall.SetWallType(6);
+    }
+  }
 }
